62e3dd778a0f679925af1c7821f30a00c262453e
Munna <jahidul.munna@northsouth.edu>
2018-09-19 02:01:29 +0600

    two dimentional matrix using vector in cpp

---
+++ 2DVector1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector< vector<int> > examples;
+int main()
+{
+    for(int i=0;i<=4;i++){
+        vector<int> temp;
+        for(int j=0;j<=4;j++){
+            temp.push_back(i);
+        }
+        examples.push_back(temp);
+    }
+
+
+
+    for(int i=0;i<=4;i++){
+        for(int j=0;j<=4;j++){
+            cout<<examples[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+}
