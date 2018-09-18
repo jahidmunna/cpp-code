fe31c03d3c898e584342f2fb9edf29db137c10f7
Munna <jahidul.munna@northsouth.edu>
2018-09-19 03:14:32 +0600

    simple continue practice in cpp

---
+++ continuePractice.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    for (int i=0;i<=3;i++){
+        if(i==0)
+            continue; //it skips printing 0;
+        cout<<i<<endl;
+    }
+}
