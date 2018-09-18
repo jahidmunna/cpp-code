660a4a177336373460d477c0d7953a938007b2f0
Munna <jahidul.munna@northsouth.edu>
2018-09-19 02:33:07 +0600

    two dimentional matrix using vector in cpp

--- 2DVector1.cpp
+++ 2DVector1.cpp
@@ -4,13 +4,17 @@ vector< vector<int> > examples;
 
 int main()
 {
+    /*insert into the vector */
     for(int i=0;i<=4;i++){
         vector<int> temp;
         for(int j=0;j<=4;j++){
             temp.push_back(i);
         }
+        //temp vector is column data in examples i-th row
         examples.push_back(temp);
     }
+
+    /*showing the result */
     for(int i=0;i<=4;i++){
         for(int j=0;j<=4;j++){
             cout<<examples[i][j]<<"\t";
