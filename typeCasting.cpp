e7b8b8e0c7925b78e7a0ec805c595a52c2f5f58a
Munna <jahidul.munna@northsouth.edu>
2018-09-29 01:07:38 +0600

    simple type casting in cpp

---
+++ typeCasting.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int x=11;
+    int b = 2;
+
+    double temp = x/b;
+    cout<<temp<<endl;
+
+    temp = (double) x/b;
+    cout<<temp<<endl;
+}
